09fe660055c72f4e87944965a8665e8b85acbb51
SoKeY <thierrypablo.schmidt@students.bfh.ch>
2018-05-08 09:09:42 +0200

    R datei von Sariar, zeigt wie man Allergy-to-pollen Code filtern kann

---
+++ SARIAR_filterCoding.R
@@ -0,0 +1,25 @@
+### SARIAR
+# Load library
+library(jsonlite)
+all.equal(mtcars, fromJSON(toJSON(mtcars)))
+
+res <- fromJSON("study6.json", simplifyDataFrame = TRUE) # simplifyDataFrame = true is default
+
+# subset auf Observation
+observation <- subset(res$entry$resource, res$entry$resource$resourceType == "Observation")
+observation$resourceType
+
+
+x <- observation$code$coding
+
+class(x)
+class(unlist(x))
+
+y <- matrix(unlist(sapply(x, as.data.frame)),ncol=3, byrow=T)
+
+y[,3]
+observation$code$coding
+
+allergyIntolerance <- subset(observation, observation$code$coding == 300910009)
+
+
