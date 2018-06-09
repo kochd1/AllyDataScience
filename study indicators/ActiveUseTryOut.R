8f13b76ce50ce9786d981d5ae15a5c3cd7395d2c
SoKeY <thierrypablo.schmidt@students.bfh.ch>
2018-06-09 13:00:42 +0200

    changes

--- study indicators/ActiveUseTryOut.R
+++ study indicators/ActiveUseTryOut.R
@@ -30,53 +30,7 @@ numOfDay <- as.integer(numOfDay)
 mydata <- fromJSON(inputFile, simplifyDataFrame = TRUE)
 
 
-
-###   3 GET SUM OF STUDY PARTICIPANTS AT MONTH X ###
-# Get all patient
-stuPart <- subset(mydata$entry$resource, mydata$entry$resource$resourceType == 'Patient')
-
-# Generate Dataframe, attribut =  {sum_StuPart, Date}
-stuPart_IdDate_df <- data.frame(sumStuPart = 0, date = 0, day = 0) # Achtung, es gibt eine Matrix mit NA, NA -> length = 2
-
-# Loop throught the month
-date <- beginDate
-while(date <= endDate) {
-  # Subset all StuPart which has been registrated at the date or earlier
-  stuPart_Id <- subset(stuPart$id, stuPart$meta$lastUpdated <= date)
-  # Get sum of these subseted StuPart
-  stuPart_Id_length <- length(stuPart_Id)
-  # Get the day of the date
-  day <- format(date, format = "%d")
-  # Generate vector with attributs = {sumOfStuPart, Date, day}
-  dateX <- as.Date(date, origin="1970-01-01")
-  stuPart_IdDate_vector <- c(stuPart_Id_length, date, day)
-  # Bind vector to dataframe
-  stuPart_IdDate_df <- rbind(stuPart_IdDate_df, stuPart_IdDate_vector)
-  
-  date <- date + 1
-}
-
-# Delete first row in matrix
-stuPart_IdDate_df <- stuPart_IdDate_df[-1,]
-
-# Make sumStuPart to integer
-stuPart_IdDate_df$sumStuPart <- as.integer(stuPart_IdDate_df$sumStuPart)
-
-# Make day to factor
-stuPart_IdDate_df$day <- as.factor(stuPart_IdDate_df$day)
-
-# The date is a number as type character
-# Make date to the format YYYY-MM-DD
-stuPart_IdDate_df$date <- as.integer(stuPart_IdDate_df$date)
-stuPart_IdDate_df$date <- as.Date.numeric(stuPart_IdDate_df$date, origin = "1970-01-01")
-
-# Show the dataframe
-stuPart_IdDate_df
-
-
-
-###   4 GET SUM OF ALL ACTIVE STUDY PARTICIPANTS AT MONTH X ###
-# Get Observation-Ressources
+### 3 GET OBSERVATION-RESOURCES  ###
 observation <- subset(mydata$entry$resource, mydata$entry$resource$resourceType == "Observation")
 
 # GET ALLERGY-TO-POLLEN OBSERVATION
@@ -87,18 +41,76 @@ y <- matrix(unlist(sapply(x, as.data.frame)),ncol=3, byrow=T) # class(y) -> matr
 # Subset only allergy-to-pollen Observation with Coding 300910009
 allergy_to_pollen <- subset(observation, y[,2] == 300910009) # class(allergy_to_pollen) -> data frame
 
-# Generate Dataframe, attribut =  {sum_activeStuPart, Date}
-activeStuPart_IdDate_df <- data.frame(activeStuPart = 0, date = 0) # Achtung, es gibt eine Matrix mit NA, NA -> length = 2
+
+
+###   4 GET SUM OF STUDY PARTICIPANTS AT MONTH X ###
+# Generate Dataframe, attribut =  {sumStuPart, date}
+stuPart_IdDate_df <- data.frame(sumStuPart = 0, date = 0) # Achtung, es gibt eine Matrix mit NA, NA -> length = 2
 
 # Loop throught the month
 date <- beginDate
 while(date <= endDate) {
-  #  PREPARE THE DATE
-  date_min <- date
-  date_max <- as.Date(date_min) + 1
-
   ##  GET ALLERGY-TO-POLLEN OBSERVATION FROM ONE SPECIFIC DAY 
-  allergy_to_pollen_time <- subset(allergy_to_pollen, allergy_to_pollen$effectiveDateTime >= date_min & allergy_to_pollen$effectiveDateTime < date_max)
+  stuPart_time <- subset(allergy_to_pollen, as.Date(allergy_to_pollen$effectiveDateTime) <= date)
+  
+  # Get length of allergy_to_pollen
+  df_length <- length(stuPart_time$id)
+  
+  # Generate empty character vector
+  patIdVector <- character()
+  
+  ##  INSERT ACTIVE STUDY PARTICIPANT INTO DATA FRAME
+  # If there is any active stuPart at this time, insert into dataframe
+  if(df_length > 0) {
+    # Fill vector patIdVector with all Patienten References
+    for (z in 1:df_length) {
+      patIdVector[z] <- stuPart_time$meta$extension[[z]]$extension[[1]]$valueReference$reference[2]
+    }
+    
+    # Eliminate the dublicates
+    uniquePatIdVector <- unique(patIdVector)
+    
+    #Get the length of the active Stu Part
+    stuPart_length <- length(uniquePatIdVector)
+    
+    # Generate vector with activeStuPart_length and date
+    stuPart_idDate_vector <- c(stuPart_length, date)
+    
+    # Add vector to dataframe
+    stuPart_IdDate_df <- rbind(stuPart_IdDate_df, stuPart_idDate_vector)
+    date <- date + 1
+  } else {
+    # Generate default vector
+    default_vector <- c(0, date)
+    
+    # Add vector to dataframe
+    activeStuPart_IdDate_df <- rbind(stuPart_IdDate_df, default_vector)
+    date <- date + 1
+  }
+}
+
+# Delete the first row 
+stuPart_IdDate_df <- stuPart_IdDate_df[-1,]
+
+# Make sumStuPart to integer
+stuPart_IdDate_df$sumStuPart <- as.integer(stuPart_IdDate_df$sumStuPart)
+
+# The date is a number as type character
+# Make date to the format YYYY-MM-DD
+stuPart_IdDate_df$date <- as.integer(stuPart_IdDate_df$date)
+stuPart_IdDate_df$date <- as.Date.numeric(stuPart_IdDate_df$date, origin = "1970-01-01")
+
+
+
+###   5 GET SUM OF ALL ACTIVE STUDY PARTICIPANTS AT MONTH X ###
+# Generate Dataframe, attribut =  {sumActiveStuPart, date}
+activeStuPart_IdDate_df <- data.frame(sumActiveStuPart = 0, date = 0) # Achtung, es gibt eine Matrix mit NA, NA -> length = 2
+
+# Loop throught the month
+date <- beginDate
+while(date <= endDate) {
+  ##  GET ALLERGY-TO-POLLEN OBSERVATION FROM ONE SPECIFIC DAY 
+  allergy_to_pollen_time <- subset(allergy_to_pollen, as.Date(allergy_to_pollen$effectiveDateTime) == date)
   
   # Get length of allergy_to_pollen
   df_length <- length(allergy_to_pollen_time$id)
@@ -121,14 +133,14 @@ while(date <= endDate) {
     activeStuPart_length <- length(uniquePatIdVector)
     
     # Generate vector with activeStuPart_length and date
-    activeStuPart_idDate_vector <- c(activeStuPart_length, date_min)
+    activeStuPart_idDate_vector <- c(activeStuPart_length, date)
     
     # Add vector to dataframe
     activeStuPart_IdDate_df <- rbind(activeStuPart_IdDate_df, activeStuPart_idDate_vector)
     date <- date + 1
   } else {
     # Generate default vector
-    default_vector <- c(0, date_min)
+    default_vector <- c(0, date)
     
     # Add vector to dataframe
     activeStuPart_IdDate_df <- rbind(activeStuPart_IdDate_df, default_vector)
@@ -140,7 +152,7 @@ while(date <= endDate) {
 activeStuPart_IdDate_df <- activeStuPart_IdDate_df[-1,]
 
 # Make sumActiveStuPart to integer
-activeStuPart_IdDate_df$activeStuPart <- as.integer(activeStuPart_IdDate_df$activeStuPart)
+activeStuPart_IdDate_df$sumActiveStuPart <- as.integer(activeStuPart_IdDate_df$sumActiveStuPart)
 
 # The date is a number as type character
 # Make date to the format YYYY-MM-DD
@@ -151,18 +163,19 @@ activeStuPart_IdDate_df$date <- as.Date.numeric(activeStuPart_IdDate_df$date, or
 activeStuPart_IdDate_df
 stuPart_IdDate_df
 
-###   5 CALCULATION ###
+###   6 CALCULATION ###
 # Calculate ratio between activeStuPart and StuPart
-ratio <- activeStuPart_IdDate_df$activeStuPart / stuPart_IdDate_df$sumStuPart
+ratio <- activeStuPart_IdDate_df$sumActiveStuPart / stuPart_IdDate_df$sumStuPart
 # Generate joined dataframe
 day <- c(1:numOfDay)
 mydf <- cbind(stuPart_IdDate_df, activeStuPart_IdDate_df, day, ratio)
-mydf <- mydf[,-(2:3), drop=FALSE]
+
+mydf <- mydf[,-2]
 mydf$day <- as.factor(mydf$day)
 mydf$date <- as.Date.character(mydf$date)
 mydf
 
-###   6 VISUALIZATION ###
+###   7 VISUALIZATION ###
 if(numOfDay <= 31) {
   p <- ggplot(data = mydf, aes(x = mydf$day, y = mydf$ratio)) + geom_point()
   p <- p + labs(x = "Day", y = "share of active StuPart", title = "Active use of the app")
@@ -174,3 +187,16 @@ if(numOfDay <= 31) {
   p <- p + theme_bw()
   p + ylim(0,1)
 }
+
+
+april <- c(4,5,3,6,2,6,8,6,9,6,8,2,4,7,9,5,7,2)
+month <- c(1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2)
+abc <- cbind(april, month)
+abc <- as.data.frame(abc)
+str(abc)
+mai <- c(6,8,2,4,7,9,5,7,2)
+abc
+
+
+
+ggplot(data = abc, aes(x = abc$month, y = abc$april)) + geom_smooth()
